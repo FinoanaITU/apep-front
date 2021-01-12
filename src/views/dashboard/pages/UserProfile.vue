b6e76e1bc099ab724ecae5183e2195e0014b4d5b
Finoana <finoanaandriatsilavoo@gmail.com>
2021-01-12 18:47:27 +0300

    card info societe

--- src/views/dashboard/pages/UserProfile.vue
+++ src/views/dashboard/pages/UserProfile.vue
@@ -69,131 +69,208 @@
         cols="12"
         md="8"
       >
-        <base-material-card>
-          <template v-slot:heading>
-            <div class="display-2 font-weight-light">
-              Edit Profile
-            </div>
-
-            <div class="subtitle-1 font-weight-light">
-              Complete your profile
-            </div>
-          </template>
-
-          <v-form>
-            <v-container class="py-0">
-              <v-row>
-                <v-col
-                  cols="12"
-                  md="4"
+        <v-form>
+          <v-container class="py-0">
+            <v-row>
+              <v-col
+                cols="12"
+                md="12"
+              >
+                <v-card
+                  elevation="10"
+                  outlined
+                  color="#324961!important"
+                  dark
                 >
-                  <v-text-field
-                    label="Company (disabled)"
-                    disabled
-                  />
-                </v-col>
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title class="headline">
+                        Information Sociéte
+                      </v-list-item-title>
+                      <v-list-item-subtitle>Siren/siret</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      md="4"
+                    >
+                      <v-text-field
+                        label="Company (disabled)"
+                        disabled
+                      />
+                    </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    class="purple-input"
-                    label="User Name"
-                  />
-                </v-col>
+                    <v-col
+                      cols="12"
+                      md="4"
+                    >
+                      <v-text-field
+                        class="purple-input"
+                        label="User Name"
+                      />
+                    </v-col>
 
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Email Address"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <v-text-field
-                    label="First Name"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <v-text-field
-                    label="Last Name"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col cols="12">
-                  <v-text-field
-                    label="Adress"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="City"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    label="Country"
-                    class="purple-input"
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    class="purple-input"
-                    label="Postal Code"
-                    type="number"
-                  />
-                </v-col>
-
-                <v-col cols="12">
-                  <v-textarea
-                    class="purple-input"
-                    label="About Me"
-                    value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                  />
-                </v-col>
-
-                <v-col
-                  cols="12"
-                  class="text-right"
-                >
-                  <v-btn
-                    color="success"
-                    class="mr-0"
+                    <v-col
+                      cols="12"
+                      md="4"
+                    >
+                      <v-text-field
+                        label="Email Address"
+                        class="purple-input"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-col>
+              <v-col
+                cols="12"
+                md="12"
+              >
+                <v-row>
+                  <v-col
+                    cols="12"
+                    md="4"
                   >
-                    Update Profile
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </base-material-card>
+                    <v-card
+                      elevation="8"
+                      color="#26c6da"
+                      dark
+                    >
+                      <v-card-title class="font-weight-light">
+                        Masse salariale
+                      </v-card-title>
+                      <v-col
+                        cols="12"
+                        md="9"
+                      >
+                        <v-text-field
+                          label="First Name"
+                          class="purple-input"
+                        />
+                      </v-col>
+                    </v-card>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="4"
+                  >
+                    <v-card
+                      elevation="10"
+                      color="#26c6da"
+                      dark
+                    >
+                      <v-card-title class="font-weight-light">
+                        Masse salariale FPC
+                      </v-card-title>
+                      <v-card-subtitle />
+                      <v-col
+                        cols="12"
+                        md="9"
+                      >
+                        <v-text-field
+                          label="First Name"
+                          class="purple-input"
+                        />
+                      </v-col>
+                    </v-card>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="4"
+                  >
+                    <v-card
+                      elevation="10"
+                      color="#26c6da"
+                      dark
+                    >
+                      <v-card-title class="font-weight-light">
+                        Masse salariale CDD
+                      </v-card-title>
+                      <v-card-subtitle />
+                      <v-col
+                        cols="12"
+                        md="9"
+                      >
+                        <v-text-field
+                          label="First Name"
+                          class="purple-input"
+                        />
+                      </v-col>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-col>
+              <v-col
+                cols="12"
+                md="6"
+              >
+                <v-text-field
+                  label="Last Name"
+                  class="purple-input"
+                />
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field
+                  label="Adress"
+                  class="purple-input"
+                />
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+              >
+                <v-text-field
+                  label="City"
+                  class="purple-input"
+                />
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+              >
+                <v-text-field
+                  label="Country"
+                  class="purple-input"
+                />
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+              >
+                <v-text-field
+                  class="purple-input"
+                  label="Postal Code"
+                  type="number"
+                />
+              </v-col>
+
+              <v-col cols="12">
+                <v-textarea
+                  class="purple-input"
+                  label="About Me"
+                  value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                />
+              </v-col>
+
+              <v-col
+                cols="12"
+                class="text-right"
+              >
+                <v-btn
+                  color="success"
+                  class="mr-0"
+                >
+                  Update Profile
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
